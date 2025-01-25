edf2d58672e8c948894549061ca904aa0bc28179
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-01-24 21:24:48 -0500

    añadir funcion para empujar burbuja con ventilador
    usa una fuerza multiplicada por el deltaTime y luego lo aplica a la burbuja con la funcion Addforce en un función update

--- ImYourLittleButterfly/Assets/Scripts/Bubble/Bubble.cs
+++ ImYourLittleButterfly/Assets/Scripts/Bubble/Bubble.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
+
+    private Rigidbody2D rigidBody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void PushBubble(Vector3 pusherPosition, float pushForce)
+    {
+
+        /* Obtenemos un vector que es igual
+        a la diferencia de posiciones entre el
+        objeto que empuja y la burbuja*/
+        Vector3 posDiff = new Vector3(transform.position.x - pusherPosition.x, transform.position.y - pusherPosition.y, 0);
+
+        // Normalizamos el vector y lo 
+        posDiff.Normalize();
+
+        // Multiplicamos el vector por una fuerza
+        posDiff *= pushForce * Time.deltaTime;
+
+        // Aplicamos la fuerza al objeto
+        rigidBody.AddForce(posDiff, ForceMode2D.Impulse);
     }
 }
--- ImYourLittleButterfly/Assets/Scripts/Pusher/PusherMovement.cs
+++ ImYourLittleButterfly/Assets/Scripts/Pusher/PusherMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PusherMovement : MonoBehaviour
 {
 
+    //=========== Limitar objeto a los límites de la pantalla ===========
     public Camera camera;
 
     private float objectWidth = 0.5f;
@@ -11,52 +12,82 @@ public class PusherMovement : MonoBehaviour
     private Vector3 screenBounds;
 
     private Vector3 mousePosition;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //=======================================================
+
+
+    //=========== Empujar burbuja ===========
+
+    private Bubble bubble;
+
+    public float pushForce = 10f;
+    //=======================================================
+
+
+    // Start se llama una vez antes de la primera ejecución de Update después de que se crea el MonoBehaviour
     void Start()
     {
-        // Get the camera bounds using ScreenToWorldPoint
+        // Obtener los límites de la cámara usando ScreenToWorldPoint
         camera = Camera.main;
 
         /* 
-        Calculate the screen bounds, through the width
-        and height of the screen, and the camera's position
+        Calcular los límites de la pantalla, a través del ancho
+        y la altura de la pantalla, y la posición de la cámara
         */
         screenBounds = camera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, camera.transform.position.z));
 
-        // Get this object's dimensions and divide them by 2
-        // this allows us to get the center of the object
+        // Obtener las dimensiones de este objeto y dividirlas por 2
+        // esto nos permite obtener el centro del objeto
         objectWidth = transform.GetComponent<Renderer>().bounds.size.x / 2;
         objectHeight = transform.GetComponent<Renderer>().bounds.size.y / 2;
 
 
+        // =========== Empujar burbuja ===========
+        // Primero, encontramos el objeto Bubble en la escena
+        bubble = FindFirstObjectByType<Bubble>();
+
 
     }
 
-    // Update is called once per frame
+    // Update se llama una vez por cuadro
     void Update()
     {
         FollowCursor();
+
+
+        // Si el botón izquierdo del ratón se mantiene presionado
+        if (Input.GetMouseButton(0))
+        {
+            PushBubble();
+        }
     }
 
-    void FollowCursor()
+    private void FollowCursor()
     {
-        // Get the mouse position
+        // Obtener la posición del ratón
         mousePosition = Input.mousePosition;
 
-        // Convert the mouse position to world position
+        // Convertir la posición del ratón a posición en el mundo
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        // Set z to 0 to keep it on the same plane   
+        // Establecer z a 0 para mantenerlo en el mismo plano   
         mousePosition.z = 0f;
 
         /* 
-        Clamp the x and y positions of the object to the screen bounds
+        Limitar las posiciones x e y del objeto a los límites de la pantalla
         */
         mousePosition.x = Mathf.Clamp(mousePosition.x, (screenBounds.x * -1) + objectWidth, screenBounds.x - objectWidth);
         mousePosition.y = Mathf.Clamp(mousePosition.y, (screenBounds.y * -1) + objectHeight, screenBounds.y - objectHeight);
 
-        // Set the object's position to the mouse position
+        // Establecer la posición del objeto a la posición del ratón
         transform.position = mousePosition;
+    }
+
+    private void PushBubble()
+    {
+        // Empujar la burbuja 
+        bubble.PushBubble(transform.position, pushForce);
+
     }
 
 }
