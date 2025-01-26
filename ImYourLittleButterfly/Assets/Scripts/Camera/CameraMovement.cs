2ccb907bc256aab151b695ae688bc36bedc7d5f2
GALIO <juandavid199er@gmail.com>
2025-01-25 21:09:10 -0500

    Ajuste movimiento camara

--- ImYourLittleButterfly/Assets/Scripts/Camera/CameraMovement.cs
+++ ImYourLittleButterfly/Assets/Scripts/Camera/CameraMovement.cs
@@ -1,16 +1,15 @@
+using TMPro;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-
-    // ============= Follow Bubble =============
+    [Header("Limites camara")]
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
     private Bubble bubble;
-
     private Vector3 bubbleXPosition;
-
     public float lerpInterpolation = 0.1f;
-
-    // ========================================
+    private float cameraHalfWidth;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +17,8 @@ public class CameraMovement : MonoBehaviour
     {
         bubble = FindFirstObjectByType<Bubble>();
 
+        // Calcular la mitad del ancho de la cámara
+        cameraHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
     }
 
     // Update is called once per frame
@@ -30,7 +31,12 @@ public class CameraMovement : MonoBehaviour
     {
         if (bubble != null)
         {
+            //Obtener la posicion de la camara
             bubbleXPosition = new Vector3(bubble.transform.position.x, transform.position.y, transform.position.z);
+
+            // Ajustar la posición deseada para mantener los bordes de la cámara dentro de los límites
+            bubbleXPosition.x = Mathf.Clamp(bubbleXPosition.x, minX + cameraHalfWidth, maxX - cameraHalfWidth);
+
             transform.position = Vector3.Lerp(transform.position, bubbleXPosition, lerpInterpolation);
         }
     }
